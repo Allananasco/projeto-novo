ff84931dc142907b6e4b3b72f2deb1c238363a10
ALANA DO NASCIMENTO <00001098869126sp@aluno.educacao.sp.gov.br>
2024-05-28 11:26:54 -0300

    atualizando 34t

---
+++ script.js
@@ -0,0 +1,7 @@
+function somarnumeros(){
+   
+    var numero1 = parseInt (document.getElementById("num1") .value);
+    var numero2 = parseInt (document.getElementById("num2") .value);
+    var resultado = numero1 + numero2
+    alert (resultado)
+}
